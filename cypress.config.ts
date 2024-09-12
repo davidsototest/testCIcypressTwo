a3e9c0d545601d2ee6a967de2adf2d2fe479ce96
David Soto <david.soto.test@gmail.com>
2024-09-12 08:05:16 -0300

    agregar el cypress-json-cucumber para linux

--- cypress.config.ts
+++ cypress.config.ts
@@ -46,3 +46,29 @@ export default defineConfig({
     },
   },
 });
+
+// import { defineConfig } from "cypress";
+// const allureWriter = require("@shelex/cypress-allure-plugin/writer");
+
+// export default defineConfig({
+//   e2e: {
+//     baseUrl: "https://demoqa.com", 
+//     video: false,
+//     screenshotOnRunFailure: false,
+//     setupNodeEvents(on, config) {
+//       allureWriter(on, config);
+//       return config;
+//     },
+//     env: {
+//       // urlFront: "https://demoqa.com/",
+//       apiUrlBack: "http://localhost:3000",
+//       mobileViewportWidthBreakpoint: 414,
+//       coverage: false,
+//       // codeCoverage: {
+//       //   url: "http://localhost:3001/__coverage__",
+//       // },
+//       // allureReuseAfterSpec: true,
+//       allureResultsPath: 'allure-results',
+//     },
+//   },
+// });
